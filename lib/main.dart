83137904a2a099bbf8c063f553cd3597107dc735
Mamta <610mamtabhatt@gmail.com>
2023-10-06 21:13:39 +0545

    List view and grid view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:flutter_application_1/widgets/button.dart";
 import "package:flutter_application_1/widgets/container_sized.dart";
+import "package:flutter_application_1/widgets/list_grid.dart";
 import "package:flutter_application_1/widgets/rowscols.dart";
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.light,
         primaryColor: Colors.deepPurple,
       ),
-      home: Button(),
+      home: ListGrid(),
       debugShowCheckedModeBanner: false,
     );
   }
---
+++ lib/widgets/list_grid.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class ListGrid extends StatefulWidget {
+  const ListGrid({super.key});
+
+  @override
+  State<ListGrid> createState() => _ListGridState();
+}
+
+class _ListGridState extends State<ListGrid> {
+  List<String> fruits = ['Orange', 'Apple', 'Mango', 'Banana'];
+  Map fruits_person = {
+    'fruits': ['Orange', 'Apple', 'Mango', 'Banana'],
+    'names': ['Karan', 'Mamta', 'Aryan', 'Laxmi']
+  };
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List and Grid"),
+        backgroundColor: Colors.red,
+        elevation: 8, //used for giving shadow
+      ),
+      body: Container(
+        // child: ListView.builder(
+        //   //Listview we can put infinite widgets and it will show all the items or anything by scrolling but this won't happen in column
+        //   itemCount: fruits.length,
+        //   itemBuilder: (context, index) {
+        //     return Card(
+        //       child: ListTile(
+        //         onTap: () {
+        //           print(fruits_person['fruits'][index]);
+        //         },
+        //         leading: Icon(Icons.person),
+        //         title: Text(fruits_person['fruits'][index]),
+        //         subtitle: Text(fruits_person['names'][index]),
+        //       ),
+        //     );
+        //   },
+        // ),
+        // child: GridView(
+        //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        //       crossAxisCount: 3,
+        //       crossAxisSpacing: 20,
+        //       mainAxisSpacing: 20,
+        //       childAspectRatio: 2 / 3),
+        //   children: [
+        //     Card(
+        //       child: Center(child: Text('Orange')),
+        //     ),
+        //     Card(
+        //       child: Center(child: Text('Orange')),
+        //     ),
+        //     Card(
+        //       child: Center(child: Text('Orange')),
+        //     ),
+        //     Card(
+        //       child: Center(child: Text('Orange')),
+        //     ),
+        //     Card(
+        //       child: Center(child: Text('Orange')),
+        //     ),
+        //     Card(
+        //       child: Center(child: Text('Orange')),
+        //     ),
+        //     Card(
+        //       child: Center(child: Text('Orange')),
+        //     ),
+        //     Card(
+        //       child: Center(child: Text('Orange')),
+        //     ),
+        //     Card(
+        //       child: Center(child: Text('Orange')),
+        //     ),
+        //   ],
+        // ),
+        child: GridView.builder(
+            itemCount: fruits.length,
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+            itemBuilder: (context, index) {
+              return Card(
+                child: Center(
+                  child: Text(fruits[index]),
+                ),
+              );
+            }),
+      ),
+    );
+  }
+}
